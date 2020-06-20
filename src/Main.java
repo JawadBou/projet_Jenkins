33bc0d5f4716d875c9a5e0ffd02360b997998879
JawadBou <60943572+JawadBou@users.noreply.github.com>
2020-06-21 01:57:59 +0200

    3 commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
 
     public static void main (String[] args){
-        System.out.println("Bonjour c'est mon premiere projet git pour jenkins ");
-        System.out.println("merci pour votre attention");
+        System.out.println("Bonjour c'est mon premiere projet git pour jenkins Trigger");
+        System.out.println("merci pour votre attention !?");
     }
 }
