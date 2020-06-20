f8310663ef258e32fc1c1dd8021d60e351b14986
JawadBou <60943572+JawadBou@users.noreply.github.com>
2020-06-21 01:47:51 +0200

    teste trigger
    2 commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
 
     public static void main (String[] args){
         System.out.println("Bonjour c'est mon premiere projet git pour jenkins ");
+        System.out.println("merci pour votre attention");
     }
 }
